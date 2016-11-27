6bd679d4628eed679fe35f7edfd9c788ff185f68
zibi-mint <zbigniew.pytel@gmail.com>
2016-11-27 00:09:32 -0500

    Implementing reducer from example-app

--- src/app/reducers/hero.reducer.ts
+++ src/app/reducers/hero.reducer.ts
@@ -4,23 +4,51 @@ import {Action} from '@ngrx/store';
 import {Hero} from '../models/hero';
 import {HeroActions} from '../actions/hero.actions'
 
-type HeroListState=Hero[];
-const initialState:HeroListState=[];
+export interface State{
+ids:number[],
+entities:{[id:number]:Hero},
+selectedHeroId: number | null
+}
+const initialState:State={
+    ids:[],
+    entities:{},
+    selectedHeroId:null
+}
 
-export const  HeroReducer=(state=initialState,action:Action):HeroListState =>{
+export const  HeroReducer=(state=initialState,action:Action):State =>{
 
     switch(action.type){
         case HeroActions.LOAD_HEROES:
         case HeroActions.LOAD_HEROES_SUCCESS:{
-            return action.payload;
+
+            const heroes=action.payload;
+            const heroesIds=heroes.map(hero=>hero.id);
+            const heroesEntities=heroes.reduce((entities:{[id:number]:Hero},hero:Hero)=>{
+                return Object.assign(entities,{
+                    [hero.id]:hero
+                });
+            },{});
+            return {
+                ids:[...state.ids,...heroesIds],
+                entities:Object.assign({},state.entities,heroesEntities),
+                selectedHeroId:state.selectedHeroId
+            };
         }
         case HeroActions.SAVE_HERO:
         case HeroActions.SAVE_HERO_SUCCESS:{
-            return [...state,action.payload];
+            const hero=action.payload;
+            const heroEntity={[hero.id]:hero}
+            return {
+                ids:[...state.ids,hero.id],
+                entities:Object.assign({},state.entities,heroEntity),
+                selectedHeroId:state.selectedHeroId
+            }
         }
         case HeroActions.UPDATE_HERO:
         case HeroActions.UPDATE_HERO_SUCCESS:{
-            return state.map(item => {
+            return Object.assi(state.entities[action.payload.id];
+
+            return state.entities[action.payload.id].map(item => {
         return item.id === action.payload.id ? Object.assign({}, item, action.payload) : item;
         })
         }
